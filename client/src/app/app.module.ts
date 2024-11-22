fd4c698f3cc624e5465af68189c33edd485b02eb
Riadh <riadhibrahim007@gmail.com>
2024-11-22 15:13:12 +0100

    Frontend setup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
   ],
   imports: [
     BrowserModule
